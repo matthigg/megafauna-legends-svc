1c9c22bec25a021d39cee2e4058c5d32273d1db3
Matt H <mhh129@gmail.com>
2023-08-01 19:38:43 -0400

    Create StudentRepository.java data access layer

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,19 +4,29 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+  private final StudentRepository studentRepository;
+
+  @Autowired
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
   
   public List<Student> getStudents() {
-		return List.of(
-			new Student(
-				1L,
-				"Mariam",
-				"mariam.jamal@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5)
-			)
-		);
+		// return List.of(
+		// 	new Student(
+		// 		1L,
+		// 		"Mariam",
+		// 		"mariam.jamal@gmail.com",
+		// 		LocalDate.of(2000, Month.JANUARY, 5)
+		// 	)
+		// );
+
+    return studentRepository.findAll();
 	}
 }
