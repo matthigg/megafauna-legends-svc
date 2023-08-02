ee702cc62e005cdcb9d1a9264681e572c226e460
Matt H <mhh129@gmail.com>
2023-08-01 20:41:29 -0400

    Create StudentService.java addNewStudent method

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,8 +1,7 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,15 +17,19 @@ public class StudentService {
   }
   
   public List<Student> getStudents() {
-		// return List.of(
-		// 	new Student(
-		// 		1L,
-		// 		"Mariam",
-		// 		"mariam.jamal@gmail.com",
-		// 		LocalDate.of(2000, Month.JANUARY, 5)
-		// 	)
-		// );
-
     return studentRepository.findAll();
 	}
+
+  public void addNewStudent(Student student) {
+    System.out.println(student);
+
+    Optional<Student> studentOptional = studentRepository.findStudentByEmail(
+      student.getEmail()
+    );
+
+    if (studentOptional.isPresent()) {
+      throw new IllegalStateException("email taken");
+    }
+    studentRepository.save(student);
+  }
 }
