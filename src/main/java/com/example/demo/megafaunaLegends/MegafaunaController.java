c52f6abc8729976f54230ccdad6235bdbe82aeda
Matt H <mhh129@gmail.com>
2023-08-02 20:04:36 -0400

    Create MegafaunaController.java controller

--- src/main/java/com/example/demo/megafaunaLegends/MegafaunaController.java
+++ src/main/java/com/example/demo/megafaunaLegends/MegafaunaController.java
@@ -1,5 +1,25 @@
 package com.example.demo.megafaunaLegends;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "megafauna")
 public class MegafaunaController {
   
+  private final MegafaunaService megafaunaService;
+
+  @Autowired
+  public megafaunaController(MegafaunaService megafaunaService) {
+    this.megafaunaService = megafaunaService;
+  }
+
+  @GetMapping
+  public List<Megafauna> getMegafauna() {
+    return MegafaunaService.getMegafauna();
+	}
 }
